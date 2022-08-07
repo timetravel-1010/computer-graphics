fb3d2158d05239ffc008589332bfcbab81847a54
LadyEO <38143022+LadyEO@users.noreply.github.com>
2022-08-06 21:36:28 -0500

    Update hsv_to_rgb.py

--- lab4/actividad-1-4/hsv_to_rgb.py
+++ lab4/actividad-1-4/hsv_to_rgb.py
@@ -1,24 +1,24 @@
 from aifc import Error
 # Formula para convertir de RGB a HSV
 def rgb_to_hsv(r, g, b): 
-r /= 255
-g /= 255 
-b /= 255 
-maxc = max(r, g, b) 
-minc = min(r, g, b) 
-v = maxc 
-if minc == maxc: return 0.0, 0.0, v 
-s = (maxc-minc) / maxc 
-rc = (maxc-r) / (maxc-minc) 
-gc = (maxc-g) / (maxc-minc) 
-bc = (maxc-b) / (maxc-minc) 
-if r == maxc: 
-    h = 0.0+bc-gc 
-    elif g == maxc:
+  r /= 255
+  g /= 255 
+  b /= 255 
+    maxc = max(r, g, b) 
+   minc = min(r, g, b) 
+  v = maxc 
+      if minc == maxc: return 0.0, 0.0, v 
+         s = (maxc-minc) / maxc 
+         rc = (maxc-r) / (maxc-minc) 
+         gc = (maxc-g) / (maxc-minc) 
+         bc = (maxc-b) / (maxc-minc) 
+                if r == maxc: 
+        h = 0.0+bc-gc 
+                   elif g == maxc:
         h = 2.0+rc-bc 
-        else: h = 4.0+gc-rc 
+                     else: h = 4.0+gc-rc 
             h = (h/6.0) % 1.0 
-            return h * 360, s * 100, v * 100
+                      return h * 360, s * 100, v * 100
         
 # Formula para convertir de HSV a RGB 
 def hsv_to_rgb(h, s, v):
