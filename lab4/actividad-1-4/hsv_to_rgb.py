eedbbe8c2463f3630c8dedaae7bfe9bdac12272a
LadyEO <38143022+LadyEO@users.noreply.github.com>
2022-08-06 21:32:33 -0500

    Update hsv_to_rgb.py

--- lab4/actividad-1-4/hsv_to_rgb.py
+++ lab4/actividad-1-4/hsv_to_rgb.py
@@ -1,5 +1,5 @@
 from aifc import Error
-# Formula para convertir de RGB to HSV
+# Formula para convertir de RGB a HSV
 def rgb_to_hsv(r, g, b): 
 r /= 255
 g /= 255 
@@ -19,7 +19,8 @@ if r == maxc:
         else: h = 4.0+gc-rc 
             h = (h/6.0) % 1.0 
             return h * 360, s * 100, v * 100
-
+        
+# Formula para convertir de HSV a RGB 
 def hsv_to_rgb(h, s, v):
     """Esta función se encarga de transformar de hsv a rgb
         debe tener en cuenta lo siguiente: 0 <= H <= 360, 0 <= s <= 1, 0 <= v <= 1"""
@@ -52,15 +53,66 @@ def hsv_to_rgb(h, s, v):
 
 print(hsv_to_rgb(360, 80, 60)) #(h, %, %)
 
-# Formula para convertir de RGB to CMY
+# Formula para convertir de RGB a CMY
 def rgb_a_cmy(r, g, b):
-  
-    # RGB values are divided by 255 
-    # to bring them between 0 to 1.
     c = 1 - r / 255
     m = 1 - g / 255
     y = 1 - b / 255
     return (c, m, y)
 
 print(rgb_a_cmy(r, g, b))
+
+# Formula para convertir de CMY a RGB
+def cmy_a_rgb(c, m, y):
+    r = 1 - c
+    g = 1 - m
+    b = 1 - y
+    return (r, g, b)
+
+print(cmy_a_rgb(c, m, y))
+
+# Formula para convertir de RGB a HLS
+def rgb_a_hsl(r, g, b):
+    r = float(r)
+    g = float(g)
+    b = float(b)
+    high = max(r, g, b)
+    low = min(r, g, b)
+    h, s, v = ((high + low) / 2,)*3
+
+    if high == low:
+        h = 0.0
+        s = 0.0
+    else:
+        d = high - low
+        s = d / (2 - high - low) if l > 0.5 else d / (high + low)
+        h = {
+            r: (g - b) / d + (6 if g < b else 0),
+            g: (b - r) / d + 2,
+            b: (r - g) / d + 4,
+        }[high]
+        h /= 6
+
+    return h, s, v
+
+# Formula para convertir de HLS a RGB
+def hsl_a_rgb(h, s, l):
+    def hue_a_rgb(p, q, t):
+        t += 1 if t < 0 else 0
+        t -= 1 if t > 1 else 0
+        if t < 1/6: return p + (q - p) * 6 * t
+        if t < 1/2: return q
+        if t < 2/3: p + (q - p) * (2/3 - t) * 6
+        return p
+
+    if s == 0:
+        r, g, b = l, l, l
+    else:
+        q = l * (1 + s) if l < 0.5 else l + s - l * s
+        p = 2 * l - q
+        r = hue_a_rgb(p, q, h + 1/3)
+        g = hue_a_rgb(p, q, h)
+        b = hue_a_rgb(p, q, h - 1/3)
+
+    return r, g, b
 
